16764c458d8c3a0dbb687d2dee5d806456fe2553
ketayao <ketayao@gmail.com>
2016-01-13 00:39:50 +0800

    删除common-io和common-utils工具依赖包。

--- src/main/java/com/ketayao/search/IndexUpdater.java
+++ src/main/java/com/ketayao/search/IndexUpdater.java
@@ -9,9 +9,10 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.ketayao.search.util.StringUtils;
 
 /**
  * 索引增量更新
@@ -36,87 +37,92 @@ import org.slf4j.LoggerFactory;
  */
 public class IndexUpdater {
 
-	private final static Logger log = LoggerFactory.getLogger(IndexUpdater.class);
-	
-	private final static Options options = new Options() {
-		/** 描述 */
-		private static final long serialVersionUID = -8014718159826725974L;
+    private final static Logger  log = LoggerFactory.getLogger(IndexUpdater.class);
 
-		{
-			addOption("p", "path", true, "Lucene index root path");
-			addOption("t", "tasker", true, "Full classname of the tasker");
-			addOption("h", "help", true, "Print this message");
-		}
-	};
-	
-	public static void main(String[] args) throws Exception {
-		execute(args);
-		System.exit(0);
-	}
-	
-	public static void execute(String[] args) throws Exception {
-		//参数解析
-		CommandLine cmd = parseCmdArgs(args);
-		String idx_path = cmd.getOptionValue('p');
-		String taskerClassname = cmd.getOptionValue('t');
-		if(StringUtils.isBlank(idx_path) || StringUtils.isBlank(taskerClassname)){
-			 new HelpFormatter().printHelp("java -Djava.ext.dirs=lib net.oschina.search.IndexUpdater [options]", options);
-			 System.exit(-1);
-		}
-		IndexTasker tasker = null;
-		try{
-			tasker = (IndexTasker)Class.forName(cmd.getOptionValue('t')).newInstance();
-		}catch(Exception e){
-			log.error("Unabled to instantiate index loader", e);
-			System.exit(-2);
-		}
-		//初始化索引管理器
-		IndexHolder holder = IndexHolder.init(idx_path);
-		
-		//处理索引更新
-		List<IndexTask> tasks = tasker.list();
+    private final static Options options;
+    static {
+        options = new Options() {
+            /** 描述 */
+            private static final long serialVersionUID = -8014718159826725974L;
 
-		for(IndexTask task : tasks) {
-			execute(holder, task, true);
-		}
+            {
+                addOption("p", "path", true, "Lucene index root path");
+                addOption("t", "tasker", true, "Full classname of the tasker");
+                addOption("h", "help", true, "Print this message");
+            }
+        };
+    }
 
-		if(tasks.size()>0)
-			log.info(tasks.size()+ " Index tasks executed finished.");
-	}
-	
-	private static CommandLine parseCmdArgs(String[] args) {
-		CommandLineParser parser = new PosixParser();
-		try {
-			return parser.parse( options, args);
-		} catch (ParseException e) {
-	        log.error("Arguments parsing failed.", e);
-		}
-		return null;
-	}
-	
-	/**
-	 * 处理单个索引任务
-	 * @param holder
-	 * @param task
-	 * @param update_status
-	 * @throws Exception
-	 */
-	private static void execute(IndexHolder holder, IndexTask task, boolean update_status) throws Exception {
-		Searchable obj = (Searchable)task.object();
-		if(obj != null){
-			switch(task.getOpt()){
-			case IndexTask.OPT_ADD:
-				holder.add(Arrays.asList(obj));
-				break;
-			case IndexTask.OPT_DELETE:
-				holder.delete(Arrays.asList(obj));
-				break;
-			case IndexTask.OPT_UPDATE:
-				holder.update(Arrays.asList(obj));
-			}
-			if(update_status)
-				task.afterBuild();
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        execute(args);
+        System.exit(0);
+    }
+
+    public static void execute(String[] args) throws Exception {
+        //参数解析
+        CommandLine cmd = parseCmdArgs(args);
+        String idx_path = cmd.getOptionValue('p');
+        String taskerClassname = cmd.getOptionValue('t');
+        if (StringUtils.isBlank(idx_path) || StringUtils.isBlank(taskerClassname)) {
+            new HelpFormatter().printHelp(
+                "java -Djava.ext.dirs=lib net.oschina.search.IndexUpdater [options]", options);
+            System.exit(-1);
+        }
+        IndexTasker tasker = null;
+        try {
+            tasker = (IndexTasker) Class.forName(cmd.getOptionValue('t')).newInstance();
+        } catch (Exception e) {
+            log.error("Unabled to instantiate index loader", e);
+            System.exit(-2);
+        }
+        //初始化索引管理器
+        IndexHolder holder = IndexHolder.init(idx_path);
+
+        //处理索引更新
+        List<IndexTask> tasks = tasker.list();
+
+        for (IndexTask task : tasks) {
+            execute(holder, task, true);
+        }
+
+        if (tasks.size() > 0)
+            log.info(tasks.size() + " Index tasks executed finished.");
+    }
+
+    private static CommandLine parseCmdArgs(String[] args) {
+        CommandLineParser parser = new PosixParser();
+        try {
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            log.error("Arguments parsing failed.", e);
+        }
+        return null;
+    }
+
+    /**
+     * 处理单个索引任务
+     * @param holder
+     * @param task
+     * @param update_status
+     * @throws Exception
+     */
+    private static void execute(IndexHolder holder, IndexTask task, boolean update_status)
+                                                                                          throws Exception {
+        Searchable obj = (Searchable) task.object();
+        if (obj != null) {
+            switch (task.getOpt()) {
+                case IndexTask.OPT_ADD:
+                    holder.add(Arrays.asList(obj));
+                    break;
+                case IndexTask.OPT_DELETE:
+                    holder.delete(Arrays.asList(obj));
+                    break;
+                case IndexTask.OPT_UPDATE:
+                    holder.update(Arrays.asList(obj));
+            }
+            if (update_status)
+                task.afterBuild();
+        }
+    }
 
 }
--- src/main/java/com/ketayao/search/SearchHelper.java
+++ src/main/java/com/ketayao/search/SearchHelper.java
@@ -8,9 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -35,70 +32,84 @@ import org.wltea.analyzer.core.IKSegmenter;
 import org.wltea.analyzer.core.Lexeme;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
+import com.ketayao.search.util.IOUtils;
+import com.ketayao.search.util.NumberUtils;
+import com.ketayao.search.util.StringUtils;
+
 /**
  * 搜索工具类
  * User: Winter Lau
  * Date: 13-1-10
  * Time: 上午11:54
  */
+@SuppressWarnings("unchecked")
 public class SearchHelper {
 
-    private final static Logger log = LoggerFactory.getLogger(SearchHelper.class);
-    private final static IKAnalyzer analyzer = new IKAnalyzer();
-	private final static BooleanQuery nullQuery = new BooleanQuery();
-    private final static Formatter highlighter_formatter = new SimpleHTMLFormatter("<span class=\"highlight\">","</span>");
+    private final static Logger       log                   = LoggerFactory
+                                                                .getLogger(SearchHelper.class);
+    private final static IKAnalyzer   analyzer              = new IKAnalyzer();
+    private final static BooleanQuery nullQuery             = new BooleanQuery();
+    private final static Formatter    highlighter_formatter = new SimpleHTMLFormatter(
+                                                                "<span class=\"highlight\">",
+                                                                "</span>");
 
-    public final static String FN_ID = "___id";
-    public final static String FN_CLASSNAME = "___class";
+    public final static String        FN_ID                 = "___id";
+    public final static String        FN_CLASSNAME          = "___class";
 
-	private final static List<String> nowords = new ArrayList<String>() {
-		/** 描述 */
-		private static final long serialVersionUID = -1566523091645361032L;
+    private final static List<String> nowords;
+    static {
+        nowords = new ArrayList<String>() {
+            /** 描述 */
+            private static final long serialVersionUID = -1566523091645361032L;
 
-		{
-			try {
-				addAll(IOUtils.readLines(SearchHelper.class
-						.getResourceAsStream("/stopword.dic")));
-			} catch (IOException e) {
-				log.error("Unabled to read stopword file", e);
-			}
-		}
-	};
+            {
+                try {
+                    addAll(IOUtils.readLines(SearchHelper.class
+                        .getResourceAsStream("/stopword.dic")));
+                } catch (IOException e) {
+                    log.error("Unabled to read stopword file", e);
+                }
+            }
+        };
+    }
 
-	private final static List<String> ReserveKeys = new ArrayList<String>() {
-		/** 描述 */
-		private static final long serialVersionUID = -4450158010640821679L;
+    private final static List<String> ReserveKeys;
+    static {
+        ReserveKeys = new ArrayList<String>() {
+            /** 描述 */
+            private static final long serialVersionUID = -4450158010640821679L;
 
-		{
-			try {
-				addAll(IOUtils.readLines(SearchHelper.class
-						.getResourceAsStream("/keywords.dic")));
-			} catch (IOException e) {
-				log.error("Unabled to read keywords file", e);
-			}
-		}
-	};
+            {
+                try {
+                    addAll(IOUtils.readLines(SearchHelper.class
+                        .getResourceAsStream("/keywords.dic")));
+                } catch (IOException e) {
+                    log.error("Unabled to read keywords file", e);
+                }
+            }
+        };
+    }
 
-	/**
-	 * 重整搜索关键短语
-	 * @param key
-	 * @return
-	 */
-	public static String cleanupKey(String key) {
-		if(ReserveKeys.contains(key.trim().toLowerCase()))
-			return key;
-		
-		StringBuilder sb = new StringBuilder();
-		List<String> keys = splitKeywords(key);
-		for(String word : keys){
-			if(sb.length() > 0)
-				sb.append(' ');
-			sb.append(word);
-		}
-		
-		return sb.toString();
-	}	
-	
+    /**
+     * 重整搜索关键短语
+     * @param key
+     * @return
+     */
+    public static String cleanupKey(String key) {
+        if (ReserveKeys.contains(key.trim().toLowerCase()))
+            return key;
+
+        StringBuilder sb = new StringBuilder();
+        List<String> keys = splitKeywords(key);
+        for (String word : keys) {
+            if (sb.length() > 0)
+                sb.append(' ');
+            sb.append(word);
+        }
+
+        return sb.toString();
+    }
+
     /**
      * 生成查询条件
      * @param field
@@ -107,24 +118,23 @@ public class SearchHelper {
      * @param machine_action
      * @return
      */
-	public static Query makeQuery(String field, String q, float boost) {
-		if(StringUtils.isBlank(q) || StringUtils.isBlank(field))
-			return nullQuery;
-		QueryParser parser = new QueryParser(Version.LUCENE_44, field, analyzer);
-		parser.setDefaultOperator(QueryParser.AND_OPERATOR);
-		try{
-			Query querySinger = parser.parse(q);
-			querySinger.setBoost(boost);
-			//System.out.println(querySinger.toString());
-			return querySinger;
-		}catch(Exception e){
-			TermQuery queryTerm = new TermQuery(new Term(field, q));
-			queryTerm.setBoost(boost);
-			//System.out.println(queryTerm.toString());
-			return queryTerm;
-		}
-	}
-	
+    public static Query makeQuery(String field, String q, float boost) {
+        if (StringUtils.isBlank(q) || StringUtils.isBlank(field))
+            return nullQuery;
+        QueryParser parser = new QueryParser(Version.LUCENE_44, field, analyzer);
+        parser.setDefaultOperator(QueryParser.AND_OPERATOR);
+        try {
+            Query querySinger = parser.parse(q);
+            querySinger.setBoost(boost);
+            //System.out.println(querySinger.toString());
+            return querySinger;
+        } catch (Exception e) {
+            TermQuery queryTerm = new TermQuery(new Term(field, q));
+            queryTerm.setBoost(boost);
+            //System.out.println(queryTerm.toString());
+            return queryTerm;
+        }
+    }
 
     /**
      * 关键字切分
@@ -135,24 +145,24 @@ public class SearchHelper {
 
         List<String> keys = new ArrayList<String>();
 
-        if(StringUtils.isNotBlank(sentence))  {
+        if (StringUtils.isNotBlank(sentence)) {
             StringReader reader = new StringReader(sentence);
             IKSegmenter ikseg = new IKSegmenter(reader, true);
-            try{
-                do{
+            try {
+                do {
                     Lexeme me = ikseg.next();
-                    if(me == null)
+                    if (me == null)
                         break;
                     String term = me.getLexemeText();
                     //if(term.length() == 1)
                     //    continue;
                     //if(StringUtils.isNumeric(StringUtils.remove(term,'.')))
                     //    continue;
-                    if(nowords.contains(term.toLowerCase()))
+                    if (nowords.contains(term.toLowerCase()))
                         continue;
                     keys.add(term);
-                }while(true);
-            }catch(IOException e){
+                } while (true);
+            } catch (IOException e) {
                 log.error("Unable to split keywords", e);
             }
         }
@@ -167,14 +177,14 @@ public class SearchHelper {
      * @return 返回格式化后的HTML文本
      */
     public static String highlight(String text, String key) {
-        if(StringUtils.isBlank(key) || StringUtils.isBlank(text))
+        if (StringUtils.isBlank(key) || StringUtils.isBlank(text))
             return text;
         String result = null;
         try {
-        	PhraseQuery pquery = new PhraseQuery();
-        	for(String sk : splitKeywords(key)){
-        		pquery.add(new Term("",QueryParser.escape(sk)));
-        	}         
+            PhraseQuery pquery = new PhraseQuery();
+            for (String sk : splitKeywords(key)) {
+                pquery.add(new Term("", QueryParser.escape(sk)));
+            }
             QueryScorer scorer = new QueryScorer(pquery);
             Highlighter hig = new Highlighter(highlighter_formatter, scorer);
             TokenStream tokens = analyzer.tokenStream(null, new StringReader(text));
@@ -182,36 +192,36 @@ public class SearchHelper {
         } catch (Exception e) {
             log.error("Unabled to hightlight text", e);
         }
-        return (result != null)?result:text;
+        return (result != null) ? result : text;
     }
-    
+
     /**
      * 返回文档中保存的对象 id
      * @param doc
      * @return
      */
     public static long docid(Document doc) {
-    	return NumberUtils.toLong(doc.get(FN_ID), 0);
+        return NumberUtils.toLong(doc.get(FN_ID), 0);
     }
-    
+
     /**
      * 获取文档对应的对象类
      * @param doc
      * @return
      * @throws ClassNotFoundException 
      */
-	public static Searchable doc2obj(Document doc) {
-    	try{
-    		long id = docid(doc);
-    		if(id <= 0)
-    			return null;
-    		Searchable obj = (Searchable)Class.forName(doc.get(FN_CLASSNAME)).newInstance();
-    		obj.setId(id);
-    		return obj;
-    	}catch(Exception e){
-    		log.error("Unabled generate object from document#id="+doc.toString(), e);
-    		return null;
-    	}
+    public static Searchable doc2obj(Document doc) {
+        try {
+            long id = docid(doc);
+            if (id <= 0)
+                return null;
+            Searchable obj = (Searchable) Class.forName(doc.get(FN_CLASSNAME)).newInstance();
+            obj.setId(id);
+            return obj;
+        } catch (Exception e) {
+            log.error("Unabled generate object from document#id=" + doc.toString(), e);
+            return null;
+        }
     }
 
     /**
@@ -220,7 +230,7 @@ public class SearchHelper {
      * @return 返回Lucene文档
      */
     public static Document obj2doc(Searchable obj) {
-        if(obj == null)
+        if (obj == null)
             return null;
 
         Document doc = new Document();
@@ -231,47 +241,47 @@ public class SearchHelper {
 
         //存储字段
         List<String> fields = obj.storeFields();
-        if(fields != null)
-            for(String fn : fields) {
+        if (fields != null)
+            for (String fn : fields) {
                 Object fv = readField(obj, fn);
-                if(fv != null)
-                	doc.add(obj2field(fn, fv, true));
+                if (fv != null)
+                    doc.add(obj2field(fn, fv, true));
             }
 
         //扩展存储字段
         Map<String, String> eDatas = obj.extendStoreDatas();
-        if(eDatas != null)
-            for(String fn : eDatas.keySet()){
-            	if(fields != null && fields.contains(fn))
-            		continue;
+        if (eDatas != null)
+            for (String fn : eDatas.keySet()) {
+                if (fields != null && fields.contains(fn))
+                    continue;
                 String fv = eDatas.get(fn);
-                if(fv != null)
-                	doc.add(obj2field(fn, fv, true));
+                if (fv != null)
+                    doc.add(obj2field(fn, fv, true));
             }
 
         //索引字段
         fields = obj.indexFields();
-        if(fields != null)
-            for(String fn : fields) {
-                String fv = (String)readField(obj, fn);
-                if(fv != null){
-                	TextField tf = new TextField(fn, fv, Field.Store.NO);
-                	tf.setBoost(obj.boost());
-                	doc.add(tf);
+        if (fields != null)
+            for (String fn : fields) {
+                String fv = (String) readField(obj, fn);
+                if (fv != null) {
+                    TextField tf = new TextField(fn, fv, Field.Store.NO);
+                    tf.setBoost(obj.boost());
+                    doc.add(tf);
                 }
             }
 
         //扩展索引字段
         eDatas = obj.extendIndexDatas();
-        if(eDatas != null)
-            for(String fn : eDatas.keySet()){
-            	if(fields != null && fields.contains(fn))
-            		continue;
+        if (eDatas != null)
+            for (String fn : eDatas.keySet()) {
+                if (fields != null && fields.contains(fn))
+                    continue;
                 String fv = eDatas.get(fn);
-                if(fv != null){
-                	TextField tf = new TextField(fn, fv, Field.Store.NO);
-                	tf.setBoost(obj.boost());
-                	doc.add(tf);
+                if (fv != null) {
+                    TextField tf = new TextField(fn, fv, Field.Store.NO);
+                    tf.setBoost(obj.boost());
+                    doc.add(tf);
                 }
             }
 
@@ -289,22 +299,24 @@ public class SearchHelper {
         try {
             return PropertyUtils.getProperty(obj, field);
         } catch (Exception e) {
-            log.error("Unabled to get property '"+field+"' of " + obj.getClass().getName(), e);
+            log.error("Unabled to get property '" + field + "' of " + obj.getClass().getName(), e);
             return null;
         }
 
     }
 
     private static Field obj2field(String field, Object fieldValue, boolean store) {
-    	if(fieldValue == null)
-    		return null;
-    	
+        if (fieldValue == null)
+            return null;
+
         if (fieldValue instanceof Date) //日期
-            return new LongField(field, ((Date)fieldValue).getTime(), store?Field.Store.YES:Field.Store.NO);
+            return new LongField(field, ((Date) fieldValue).getTime(), store ? Field.Store.YES
+                : Field.Store.NO);
         if (fieldValue instanceof Number) //其他数值
-            return new StringField(field, String.valueOf(((Number)fieldValue).longValue()), store?Field.Store.YES:Field.Store.NO);
+            return new StringField(field, String.valueOf(((Number) fieldValue).longValue()),
+                store ? Field.Store.YES : Field.Store.NO);
         //其他默认当字符串处理
-        return new StringField(field, (String)fieldValue, store?Field.Store.YES:Field.Store.NO);
+        return new StringField(field, (String) fieldValue, store ? Field.Store.YES : Field.Store.NO);
     }
 
 }
